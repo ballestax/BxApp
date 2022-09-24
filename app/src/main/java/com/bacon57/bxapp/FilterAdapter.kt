e1a10066290793f133cd5d3997639949892275d4
LuisR <ballestax@gmail.com>
2022-09-24 15:53:23 -0500

    No navigation, manejo de clicks

--- app/src/main/java/com/bacon57/bxapp/FilterAdapter.kt
+++ app/src/main/java/com/bacon57/bxapp/FilterAdapter.kt
@@ -1,41 +1,44 @@
 package com.bacon57.bxapp
 
-import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.BaseAdapter
-import android.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.bacon57.bxapp.databinding.ProductFilterBinding
+import com.bacon57.bxapp.databinding.AdapterCategoryBinding
+import com.bacon57.bxapp.databinding.AdapterProductBinding
+import com.squareup.picasso.Picasso
+import java.text.NumberFormat
 
-class FilterAdapter(private val context: Context) : BaseAdapter(){
+class FilterAdapter: RecyclerView.Adapter<CategoryViewHolder>(){
 
     var categories = mutableListOf<Category>()
 
-    fun setCategoryList(categories: List<Category>){
+    fun setCategoriesList(categories: List<Category>){
         this.categories = categories.toMutableList()
         notifyDataSetChanged()
     }
 
-    override fun getCount(): Int {
-        return categories.size
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = AdapterCategoryBinding.inflate(inflater, parent, false)
+        return CategoryViewHolder(binding)
     }
 
-    override fun getItem(position: Int): Any {
-        return position
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+        val category = categories[position]
+//        holder.binding
+        holder.render(category)
     }
 
-    override fun getItemId(position: Int): Long {
-        return position.toLong()
+    override fun getItemCount(): Int {
+        return  categories.size
+    }
+}
+
+class CategoryViewHolder(val binding: AdapterCategoryBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    fun render(category: Category) {
+        binding.tvFilterName.text = category.name
+        // binding.btAdd.setOnClickListener{ onClickListener(product) }
     }
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        var convertView = convertView
-        val inflater = LayoutInflater.from(parent?.context ?: context)
-        val binding = ProductFilterBinding.inflate(inflater, parent, false)
-        binding.tvFilterName.text = categories.get(position).name
-        return binding.root
-    }
 }
--- app/src/main/java/com/bacon57/bxapp/ProductListViewModel.kt
+++ app/src/main/java/com/bacon57/bxapp/ProductListViewModel.kt
@@ -1,21 +1,44 @@
 package com.bacon57.bxapp
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class ProductListViewModel(private val repository: ProductsRepository, private val catRepository: CategoriesRepository): ViewModel() {
+class ProductListViewModel(
+    private val repository: ProductsRepository,
+    private val catRepository: CategoriesRepository
+) :
+    ViewModel() {
 
     val productList = MutableLiveData<List<Product>>()
     val categoriesList = MutableLiveData<List<Category>>()
+    val _progressVisible = MutableLiveData<Boolean>()
+    val progressVisible: LiveData<Boolean> get() = _progressVisible
+
 
     val errorMessage = MutableLiveData<String>()
 
-    fun getAllProducts(){
+    fun onCreate() {
+        viewModelScope.launch {
+            _progressVisible.value = true
+            getAllProducts()
+            getAllCategories()
+            _progressVisible.value = false
+        }
+    }
+
+    fun onProductCLick(product: Product) {
+
+    }
+
+    fun getAllProducts() {
         val response = repository.getAllProducts()
-        response.enqueue(object : Callback<List<Product>>{
+        response.enqueue(object : Callback<List<Product>> {
             override fun onResponse(call: Call<List<Product>>, response: Response<List<Product>>) {
                 productList.postValue(response.body())
             }
@@ -27,10 +50,13 @@ class ProductListViewModel(private val repository: ProductsRepository, private v
         )
     }
 
-    fun getAllCategories(){
+    fun getAllCategories() {
         val response = catRepository.getAllCategories()
-        response.enqueue(object : Callback<List<Category>>{
-            override fun onResponse(call: Call<List<Category>>, response: Response<List<Category>>) {
+        response.enqueue(object : Callback<List<Category>> {
+            override fun onResponse(
+                call: Call<List<Category>>,
+                response: Response<List<Category>>
+            ) {
                 categoriesList.postValue(response.body())
             }
 
